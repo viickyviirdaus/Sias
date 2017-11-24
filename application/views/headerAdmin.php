8fd93d58ac29f9a409348bcc2f1d07184a36cf8e
Ade Wija Nugraha <adewijanugraha@Ade-Wija-Nugraha.local>
2017-11-24 12:51:20 +0700

    no message

--- application/views/headerAdmin.php
+++ application/views/headerAdmin.php
@@ -5,9 +5,8 @@
 	<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets'); ?>/css/cssnyatemplate.css"> -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets'); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url('assets'); ?>/font-awesome-4.7.0/css/font-awesome.min.css">
-    <script type="text/javascript" src="<?php echo base_url('assets'); ?>/js/jquery-3.1.1.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets'); ?>/js/popper.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets'); ?>/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets'); ?>/jquery/jquery-3.2.1.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets'); ?>/js/bootstrap.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" style="height: 60px">
@@ -31,7 +30,7 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Wali Kelas <span class="caret"></span></a>
 						<ul class="dropdown-menu">
-							<li><a id="buatAkunWaliKelas" href="#">Buat Akun Wali Kelas</a></li>
+							<li><a href="#" id="buatAkunWaliKelas">Buat Akun Wali Kelas</a></li>
 							<li><a href="<?php echo base_url('index.php/Controller_waliKelas/tampilWaliKelasAktif'); ?>">Lihat Akun Wali Kelas Aktif</a></li>
 							<li><a href="<?php echo base_url('index.php/Controller_waliKelas/tampilWaliKelasTidakAktif'); ?>"">Lihat Akun Wali Kelas Non Aktif</a></li>
 							<!-- <li role="separator" class="divider"></li> -->
@@ -64,6 +63,7 @@
 				</ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
+        <div class="container">
 	</nav>
 
 	<!-- Modal -->
@@ -147,3 +147,5 @@
             });
 
         </script>
+
+        
